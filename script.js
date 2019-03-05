bcdfac79e9acd74d5febf7702fd8e78eeb5cd2ed
emilymorton <43576998+emilymorton@users.noreply.github.com>
2019-03-05 09:24:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$(".Spell1").click(function(){
+    $(".smurf").attr("src","http://coolspotters.com/files/photos/1093547/vexy-gallery.png");
+ 
+});
+
+
+
+//Spell #2
+
+$(".Spell2").click(function(){
+    $(".burger").attr("src","https://static.tvtropes.org/pmwiki/pub/images/image_138.jpeg");
+ 
+});
+
+
+
+//Spell #3
+
+$(".Spell3").click(function(){
+    $(".switch").fadeOut();
+ 
+});
+
+
+
